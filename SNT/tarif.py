98aaf35158b9dc151897471f9dc5d1d8617bf61e
Hugo <24863815+wheredidhugo@users.noreply.github.com>
2022-04-08 13:36:39 +0200

    Update tarif.py

--- SNT/tarif.py
+++ SNT/tarif.py
@@ -15,15 +15,11 @@ if iNumPassagers == 0:
 
 iPrix = int(input("Prix plein tarif de l'excursion? (en euros) "))
 
-if iNumPassagers == 1:
-  iAge = int(input("Âge du passager? (en années) "))
-  print("Tarif total de l'excursion: " + str(tarif(iAge, iPrix)))
-else:
-  tarifTotal = 0;
-  for i in range(iNumPassagers):
-    iAge = int(input("Âge du passager " + str(i) + "? (en années) "))
-    tarifPassager = tarif(iAge, iPrix)
-    print("Tarif pour le passager " + str(i) + ": " + str(tarifPassager))
-    if tarifPassager != "Gratuit":
-      tarifTotal = tarifTotal + tarifPassager
-  print("Tarif total de l'excursion: " + str(tarifTotal))
+tarifTotal = 0;
+for i in range(iNumPassagers):
+  iAge = int(input("Âge du passager " + str(i+1) + "? (en années) "))
+  tarifPassager = tarif(iAge, iPrix)
+  print("Tarif pour le passager " + str(i+1) + ": " + str(tarifPassager))
+  if tarifPassager != "Gratuit":
+    tarifTotal = tarifTotal + tarifPassager
+print("Tarif total de l'excursion: " + str(tarifTotal))
